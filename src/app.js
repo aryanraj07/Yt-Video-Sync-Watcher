72e7ac3db14fb8d09abb62048c56a938cd8fdc12
aryanraj07 <aryanraj010010@gmail.com>
2025-09-07 01:54:12 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ import friendRoutes from "./routes/friendRoutes.js";
 app.use(express.json({ limit: "16kb" }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL?.split(",") || ["http://localhost:5173"],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
+console.log(process.env.FRONTEND_URL?.split(","));
+
 app.use(morgan());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
